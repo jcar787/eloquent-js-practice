dae5e6281d01e9a85141d6e503e5c12a170f223a
Jeff C <blackone787@gmail.com>
2018-11-02 22:30:36 -0500

    Making the exercises from chapter 2 into functions.

--- 02-program-structure/chessboard.js
+++ 02-program-structure/chessboard.js
@@ -1,14 +1,19 @@
-const size = 8
-
-for (let i = 0; i<size; i++) {
-  let line = ''
-  let flag = i % 2
-  for (let j=0; j<size; j++) {
-    if (j % 2 == flag) {
-      line += ' '
-    } else {
-      line += '#'
+const chessboard = (size = 8) => {
+  for (let i = 0; i < size; i++) {
+    let line = '';
+    let flag = i % 2;
+    for (let j = 0; j < size; j++) {
+      if (j % 2 == flag) {
+        line += ' ';
+      } else {
+        line += '#';
+      }
     }
+    console.log(line);
   }
-  console.log(line)
-}
+};
+
+chessboard();
+console.log();
+console.log();
+chessboard(9);
--- 02-program-structure/fizzbuzz.js
+++ 02-program-structure/fizzbuzz.js
@@ -1,15 +1,19 @@
-for(let i=1; i<=100; i++){
-  let result = ''
-  if (i % 3 == 0) {
-    result += 'Fizz'
-  }
-  if (i % 5 == 0) {
-    result += 'Buzz'
-  }
+const fizzbuzz = () => {
+  for (let i = 1; i <= 100; i++) {
+    let result = '';
+    if (i % 3 == 0) {
+      result += 'Fizz';
+    }
+    if (i % 5 == 0) {
+      result += 'Buzz';
+    }
 
-  if (result) {
-    console.log(result)
-  } else {
-    console.log(i)
+    if (result) {
+      console.log(result);
+    } else {
+      console.log(i);
+    }
   }
-}
+};
+
+fizzbuzz();
